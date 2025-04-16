00aceae3de628c64af47f7fccd1c027d54f1d8e0
MYCOMPUTER\fuxav <kotake.ukyou.t5@dc.tohoku.ac.jp>
2025-04-17 01:08:07 +0900

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -128,7 +128,7 @@ function App() {
               {
                 name: '佐藤 花子',
                 role: '副代表理事',
-                image: 'https://github.com/yanbarukuinaUk/volunteer-organization/blob/master/public/images/pic.png',
+                image: 'https://github.com/yanbarukuinaUk/volunteer-organization/blob/master/public/images/pic.png?raw=true',
               },
               {
                 name: '鈴木 一郎',
